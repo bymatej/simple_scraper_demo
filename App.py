069de29578f698a102beb77017868614004e8470
Matej Jelić <programming@bymatej.com>
2020-06-30 09:33:02 +0200

    Add another URL for reading, missing regex to get li tags

--- App.py
+++ App.py
@@ -1,11 +1,19 @@
 import urllib.request
 import urllib.parse
+import re
 
-url = "https://google.com/search"
-parameters = {"q": "python"}
+url = "https://ut.abfackeln.com/asu.html"
+parameters = {"page": "install"}
 data = urllib.parse.urlencode(parameters)
-requested_data = data.encode("utf-8")
-request = urllib.request.Request(url, requested_data)
+# requested_data = data.encode("utf-8")
+print(data)
+full_url = url + "?" + data
+print(full_url)
+
+request = urllib.request.Request(full_url)
 response = urllib.request.urlopen(request)
 response_data = response.read()
 print(response_data)
+
+text_in_li_tags = re.findall(r"", str(response_data), re.IGNORECASE)
+print(text_in_li_tags)
